73331f90484bf055dfb37553e30ff934f0ba3bf2
Mart277h <mlj@hotmail.dk>
2019-09-05 14:08:42 +0200

    First class

---
+++ First class.R
@@ -0,0 +1,9 @@
+#excercise 2
+
+table_names<-c("Kristine","Sarah","Lærke","Kristian","Martine")
+table_siblings<-c(3,1,2,3)
+
+data.frame<-c(Name=table_names,Siblings=table_siblings)
+df
+View(df)
+View(data.frame)
